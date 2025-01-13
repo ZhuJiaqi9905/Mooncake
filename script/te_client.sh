ecbc260ae6c84e5ceab3f77d1d500ff186dfdf22
ZhuJiaqi <zhujiaqi9905@gmail.com>
2025-01-13 11:25:31 +0800

    change client to redis

---
+++ script/te_client.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+export MC_GID_INDEX=0
+export PATH=/usr/local/cuda/bin:$PATH
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64:/usr/local/lib:$LD_LIBRARY_PATH
+export LIBRARY_PATH=$LIBRARY_PATH:/usr/local/cuda/lib64
+export CUDA_PATH=/usr/local/cuda
+./build/mooncake-transfer-engine/example/transfer_engine_bench \
+    --metadata_server=10.20.23.91:2379 \
+    --segment_id=172.21.0.91:12345 \
+    --local_server_name=172.21.0.92:12346 \
+    --device_name=mlx5_0 \
+    --use_vram=false \
+    -metadata_type redis \
